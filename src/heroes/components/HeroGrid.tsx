4b94a1f7884581e2f46a4598be1db175ddbfcc0e
gpere13 <gpere13@bancodebogota.com.co>
2026-06-11 20:18:47 -0500

    Grid heroes and tabs

---
+++ src/heroes/components/HeroGrid.tsx
@@ -0,0 +1,18 @@
+import { HeroGridCard } from "./HeroGridCard"
+
+export const HeroGrid = () => {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-8">
+            < HeroGridCard />
+            < HeroGridCard />
+            < HeroGridCard />
+            < HeroGridCard />
+            < HeroGridCard />
+            < HeroGridCard />
+            < HeroGridCard />
+            < HeroGridCard />
+            < HeroGridCard /> < HeroGridCard />
+
+        </div >
+    )
+}
--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,5 +1,6 @@
 import { CustomJumbotron } from "@/components/custom/CustomJumbotron"
 import { HeroStats } from "@/heroes/components/HeroStats"
+import { SearchControls } from "./ui/SearchControls"
 
 export const SearchPage = () => {
     return (
@@ -9,6 +10,7 @@ export const SearchPage = () => {
                 description="Descubre, edxplora y administra super heroes y villanos"
             />
             <HeroStats />
+            <SearchControls />
         </>
     )
 }
