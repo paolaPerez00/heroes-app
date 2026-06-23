af1ba06b2a5cb6a8715a37e965b2dc04e5462e5a
gpere13 <gpere13@bancodebogota.com.co>
2026-06-23 17:00:39 -0500

    Add functional filters

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -2,8 +2,30 @@ import { CustomJumbotron } from "@/components/custom/CustomJumbotron"
 import { HeroStats } from "@/heroes/components/HeroStats"
 import { SearchControls } from "./ui/SearchControls"
 import { CustomBreadcrumb } from "@/components/custom/CustomBreadcrumb"
+import { HeroGrid } from "@/heroes/components/HeroGrid"
+import { useQuery } from "@tanstack/react-query"
+import { searchHerosAction } from "@/heroes/actions/search-heros.action"
+import { useSearchParams } from "react-router"
 
 export const SearchPage = () => {
+
+    const [searchParams] = useSearchParams();
+
+    const options = {
+        team: searchParams.get('team') ?? '',
+        universe: searchParams.get('universe') ?? '',
+        name: searchParams.get('name') ?? '',
+        category: searchParams.get('category') ?? '',
+        status: searchParams.get('status') ?? '',
+        strength: searchParams.get('strength') ?? ''
+    }
+
+    const { data = [] } = useQuery({
+        queryKey: ['heroes', 'search', { options }],
+        queryFn: () => searchHerosAction(options),
+        staleTime: 1000 * 60 * 5
+    })
+
     return (
         <>
             <CustomJumbotron
@@ -22,6 +44,7 @@ export const SearchPage = () => {
             />
             <HeroStats />
             <SearchControls />
+            <HeroGrid heroes={data} />
         </>
     )
 }
