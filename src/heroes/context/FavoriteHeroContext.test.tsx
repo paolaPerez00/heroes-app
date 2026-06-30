ff48da1402b0bd6e0170ff1031abfbdfb21f47f9
gpere13 <gpere13@bancodebogota.com.co>
2026-06-30 09:57:20 -0500

    Add test of context

---
+++ src/heroes/context/FavoriteHeroContext.test.tsx
@@ -0,0 +1,77 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { beforeEach, describe, expect, test } from "vitest";
+import { FavoriteHeroContext, FavoriteHeroProvider } from "./FavoriteHeroContext";
+import { use } from "react";
+import type { Hero } from "../types/hero.interface";
+
+const mockHero = {
+    id: '1',
+    name: 'batman'
+} as Hero;
+
+
+const TestComponent = () => {
+    const { favoriteCount, favorites, isFavorite, toogleFavorite } = use(FavoriteHeroContext);
+
+    return (
+        <div>
+            <div data-testid="favorite-count">{favoriteCount}</div>
+            <div data-testid="favorite-list">
+                {
+                    favorites.map(hero => (
+                        <div key={hero.id} data-testid={`hero-${hero.id}`}>
+                            {hero.name}
+                        </div>
+                    ))
+                }
+            </div>
+            <button data-testid="toogle-favorite" onClick={() => toogleFavorite(mockHero)}>
+                Toogle Favorite
+            </button>
+            <div data-testid="is-favorite">
+                {isFavorite(mockHero).toString()}
+            </div>
+        </div>
+    )
+}
+
+const renderContextTest = () => {
+    return render(
+        < FavoriteHeroProvider >
+            <TestComponent />
+        </FavoriteHeroProvider >
+    )
+}
+
+describe('FavoriteHeroContext', () => {
+
+    beforeEach(() => {
+        localStorage.clear();
+    })
+    test('should initialize with default values', () => {
+        renderContextTest();
+        expect(screen.getByTestId('favorite-count').textContent).toBe('0');
+        expect(screen.getByTestId('favorite-list').children.length).toBe(0);
+    })
+
+    test('should add hero to favorites when toogleFavorite is called with new Hero', () => {
+        renderContextTest();
+        const button = screen.getByTestId('toogle-favorite');
+        fireEvent.click(button);
+        expect(screen.getByTestId('is-favorite').textContent).toBe('true');
+        expect(screen.getByTestId('favorite-count').textContent).toBe('1');
+        expect(screen.getByTestId('hero-1').textContent).toBe('batman');
+        expect(localStorage.getItem('favorites')).toBe('[{"id":"1","name":"batman"}]')
+    })
+    test('should remove hero to favorites when toogleFavorite is called', () => {
+        localStorage.setItem('favorites', JSON.stringify([mockHero]))
+        renderContextTest();
+        expect(screen.getByTestId('favorite-count').textContent).toBe('1');
+        const button = screen.getByTestId('toogle-favorite');
+        fireEvent.click(button);
+
+        expect(screen.getByTestId('favorite-count').textContent).toBe('0');
+        expect(screen.getByTestId('is-favorite').textContent).toBe('false');
+        expect(screen.queryByTestId('hero-1')).toBeNull();
+    })
+})
